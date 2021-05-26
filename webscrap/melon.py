91b6106696ede56b7451363ad4e21a3f4121cd5b
unknown <kimdongju3331@gmail.com>
2021-05-26 16:18:47 +0900

    melon 차트 크롤링

---
+++ webscrap/melon.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class Melon(object):
+    URL = 'https://www.melon.com/chart/index.htm'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    titles = ''
+    artists = ''
+
+    # req_melon
+    def req_melon(self):
+        request = requests.get(self.URL, headers=self.headers).text
+        return BeautifulSoup(request, 'lxml')
+
+    # 곡명(title), 가수(artist) 순위별, 앨범(album) 순위별
+    def get_chart_all(self):
+        for i in range(len(self.titles)):
+            title = self.titles[i].text.strip().split('\n')[0]
+            artist = self.artists[i].text.strip().split('\n')[0]
+            print('{:3}위 {} - {}'.format(i + 1, artist, title))
+
+    @staticmethod
+    def main():
+        m = Melon()
+        soup = m.req_melon()
+        m.titles = soup.select('div.rank01')
+        print(len(m.titles))
+        m.artists = soup.select('div.rank02>span')
+        print(len(m.artists))
+        m.get_chart_all()
+
+
+Melon.main()
